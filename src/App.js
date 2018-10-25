dc8be8235d3ff1d3b401b13a7e53326f273149be
Lauren Weaver <lbrickner@Chriss-Air.lan>
2018-10-25 11:50:08 -0600

    set up bootstrap with custom theme

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 import { Button } from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <header className="App-header bg-accent1">
           Southwest Region Woman's Club
           <Button outline size="lg" color="primary">Woo!</Button>
         </header>
